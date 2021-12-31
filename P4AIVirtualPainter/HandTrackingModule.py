c9159049347322e267491452e42fb8f7c334cee7
CChCheChen <chen.lin.0404@gmail.com>
2021-12-30 23:39:55 -0500

    AI Virtual Painter

--- P4AIVirtualPainter/HandTrackingModule.py
+++ P4AIVirtualPainter/HandTrackingModule.py
@@ -13,6 +13,13 @@ class handDetector():
         self.hands = self.mpHands.Hands(self.mode, self.maxHands, self.modelComplexity, self.detectionConf, self.trackConf) # modify the parameter if needed
         self.mpDraw = mp.solutions.drawing_utils
 
+        self.tipIDs = [4, 8, 12, 16, 20]
+        # 4 for thumb
+        # 8 for index finger
+        # 12 for middle finger
+        # 16 for ring finger
+        # 20 for pinky finger
+
     def findHands(self, img, draw=True):
         imgRGB = cv.cvtColor(img, cv.COLOR_BGR2RGB)  # hands object only uses RGB images
         self.results = self.hands.process(imgRGB)
@@ -24,7 +31,7 @@ class handDetector():
         return img
 
     def findPosition(self, img, handNumb=0, draw=True):
-        lmList = []
+        self.lmList = []
         if self.results.multi_hand_landmarks:
             thisHand = self.results.multi_hand_landmarks[handNumb]
             for id, lm in enumerate(thisHand.landmark):
@@ -32,10 +39,28 @@ class handDetector():
                 h, w, c = img.shape
                 cx, cy = int(lm.x * w), int(lm.y * h)  # position of center
                 # print(id, cx, cy)
-                lmList.append([id, cx, cy])
+                self.lmList.append([id, cx, cy])
                 if draw:
                     cv.circle(img, (cx, cy), 7, (255, 0, 0), cv.FILLED)
-        return lmList
+        return self.lmList
+
+    def fingersUp(self):
+        fingers = []
+        # For thumb
+        # if lmList[tipIDs[0]][1] < lmList[tipIDs[0] - 1][1]: # Only works for RIGHT hand when image flipped
+        if self.lmList[self.tipIDs[0]][1] > self.lmList[self.tipIDs[0] - 1][1]:  # Only works for LEFT hand when image flipped
+            fingers.append(1)
+        else:
+            fingers.append(0)
+
+        # For other fingers
+        for id in range(1, 5):
+            # if lmList[tipIDs[id]][2] > lmList[tipIDs[id] - 2][2]: # Only works for RIGHT hand
+            if self.lmList[self.tipIDs[id]][2] < self.lmList[self.tipIDs[id] - 2][2]:  # Only works for LEFT hand
+                fingers.append(1)
+            else:
+                fingers.append(0)
+        return fingers
 
 def main():
     pTime = 0
--- P4AIVirtualPainter/VirtualPainter.py
+++ P4AIVirtualPainter/VirtualPainter.py
@@ -3,10 +3,12 @@ import numpy as np
 import time
 import os
 
-import  HandTrackingModule as htm
+import HandTrackingModule as htm
 
 ##########################
-wCam, hCam = 1240, 480
+wCam, hCam = 1280, 720
+brushThickness = 15
+eraserThickness = 50
 ##########################
 
 capture = cv.VideoCapture(0)
@@ -22,27 +24,86 @@ for imPath in myList:
     image = cv.imread(f'{folderPath}/{imPath}')
     # print(f'{folderPath}/{imPath}')
     overLayList.append(image)
+# print(len(overLayList))
+header = overLayList[3]
+drawColor = (0, 0, 255)
 
-print(len(overLayList))
+detector = htm.handDetector(detectionConf=0.85)
 
-pTime = 0
-cTime = 0
+xp, yp = 0, 0
 
-detector = htm.handDetector(detectionConf=0.75)
+imgCanvas = np.zeros((hCam, wCam, 3), np.uint8)
 
 while True:
+    # 1. Import image
     success, img = capture.read()
     if success:
+        # 1a. Flip the image
+        img = cv.flip(img, 1)
+
+        # 2. Find hand landmarks
         img = detector.findHands(img)
         lmList = detector.findPosition(img, draw=False)
-        # print(lmList)
+        if len(lmList) != 0:
+            # print(lmList)
+            # Find tips of index finger and middle finger
+            x1, y1 = lmList[8][1:]
+            x2, y2 = lmList[12][1:]
 
-        cTime = time.time()
-        fps = 1/(cTime - pTime)
-        pTime = cTime
-        cv.putText(img, f'FPS:{int(fps)}', (1000, 70), cv.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 2)
+            # 3. Check which finger(s) is up or not. Draw when index finger is up; Select color when 2 fingers are up.
+            fingers = detector.fingersUp()
+            # print(fingers)
 
-        cv.imshow("Result", img)
+            # 4. If 2 fingers are up (in Selection mode)
+            if fingers[1] and fingers[2]:
+                xp, yp = 0, 0
+                # print("Selection Mode")
+                # Checking for the click
+                if y1 < 125:
+                    if 140 < x1 < 270: # red
+                        header = overLayList[3]
+                        drawColor = (0, 0, 255)
+                    elif 430 < x1 < 560: # blue
+                        header = overLayList[0]
+                        drawColor = (255, 0, 0)
+                    elif 720 < x1 < 850: # green
+                        header = overLayList[2]
+                        drawColor = (0, 255, 0)
+                    elif 1030 < x1 < 1170: # eraser
+                        header = overLayList[1]
+                        drawColor = (0, 0, 0)
+                # cv.rectangle(img, (x1, y1 - 15), (x2, y2 + 15), drawColor, cv.FILLED)
+
+            # 5. If 1 finger (index finger) is up (in Draw mode)
+            if fingers[0]==False and fingers[1] and fingers[2]==False and fingers[3]==False and fingers[4]==False:
+                # print("Draw Mode")
+                cv.circle(img, (x1, y1), 15, drawColor, cv.FILLED)
+                if xp == 0 and yp == 0:
+                    xp, yp = x1, y1
+
+                if drawColor == (0, 0, 0):
+                    cv.line(img, (xp, yp), (x1, y1), drawColor, eraserThickness)
+                    cv.line(imgCanvas, (xp, yp), (x1, y1), drawColor, eraserThickness)
+                else:
+                    cv.line(img, (xp, yp), (x1, y1), drawColor, brushThickness)
+                    cv.line(imgCanvas, (xp, yp), (x1, y1), drawColor, brushThickness)
+                xp, yp = x1, y1
+
+        if len(lmList) == 0:
+            xp, yp = 0, 0
+            
+        imgGray = cv.cvtColor(imgCanvas, cv.COLOR_BGR2GRAY)
+        _, imgInverse = cv.threshold(imgGray, 50, 255, cv.THRESH_BINARY_INV)
+        imgInverse = cv.cvtColor(imgInverse, cv.COLOR_GRAY2RGB)
+        img = cv.bitwise_and(img, imgInverse)
+        img = cv.bitwise_or(img, imgCanvas)
+
+        # Setting the header image
+        img[0:125, 0:1280] = header
+        # img = cv.addWeighted(img, 0.5, imgCanvas, 0.5, 0)
+
+        cv.imshow("Img", img)
+        # cv.imshow("ImgCanvas", imgCanvas)
 
         if cv.waitKey(1) & 0xFF == ord('q'):
             break
